fcf307aaa41c666caa39bb626dd4a09314092c67
AshleyCol <ashe.coleman1022@gmail.com>
2026-04-28 23:42:33 -0400

    cleared

--- Chapter 3 Project/Chapter 3 Project.cpp
+++ Chapter 3 Project/Chapter 3 Project.cpp
@@ -4,19 +4,7 @@ using namespace std;
 
 int main()
 {
-    const double sugar = 1.5;
-    const double butter = 1.0;
-    const double flour = 2.75;
-    double cookies;
-
-    cout << "How many cookies would you like? \n";
-
-    cin >> cookies;
-
-    cookies = cookies / 48;
-
-    cout << "You need " << sugar * cookies << " cups of sugar, " << butter * cookies << " cups of butter, and " << flour * cookies << " cups of flour.";
-
+    
     
     return 0;
 }
