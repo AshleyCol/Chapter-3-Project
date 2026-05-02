9764df39eb0c7c3a3b4ffe7c4398f9a91d5d707d
AshleyCol <ashe.coleman1022@gmail.com>
2026-05-01 20:40:19 -0400

    Math Tutor Complete

--- Chapter 3 Project/Chapter 3 Project.cpp
+++ Chapter 3 Project/Chapter 3 Project.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
+#include <random>
 
 using namespace std;
 
 int main()
 {
-    
-    
+    // Randomness
+    random_device engine;
+    uniform_int_distribution<int> randomInt(0, 100);
+
+    // Get numbers & solve
+    int randFirst = randomInt(engine);
+    int randSecond = randomInt(engine);
+
+    int solved = randFirst + randSecond;
+
+    // OUtput & wait
+    cout << "Please solve " << randFirst << " + " << randSecond << "\n";
+    cin.get();
+    cout << solved;
+
     return 0;
 }
